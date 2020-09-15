fe7d6609b905db4e84aefb08f7c12481556ea59b
leeyunhome <leeyunhome@daum.net>
2020-09-16 05:49:55 +0900

    1_12 헤더 가드가 필요한 이유

---
+++ Chapter1/1_1 헤더파일 만들기/1_1 헤더파일 만들기.cpp
@@ -0,0 +1,14 @@
+﻿// 1_1 헤더파일 만들기.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "add.h"
+
+using namespace std;
+
+int main()
+{
+	cout << add(1, 2) << endl;
+
+	return 0;
+}
---
+++ Chapter1/1_12 헤더 가드가 필요한 이유/1_12 헤더 가드가 필요한 이유.cpp
@@ -0,0 +1,9 @@
+﻿// 1_12 헤더 가드가 필요한 이유.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
