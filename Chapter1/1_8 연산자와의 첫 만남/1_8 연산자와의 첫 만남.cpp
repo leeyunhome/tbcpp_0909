d6495cec216ae46d94702fe6dc1de0550248e7ff
leeyunhome <leeyunhome@daum.net>
2020-09-16 04:38:41 +0900

    1.9 기본적인 서식 맞추기

---
+++ Chapter1/1_8 연산자와의 첫 만남/1_8 연산자와의 첫 만남.cpp
@@ -0,0 +1,10 @@
+﻿// 1_8 연산자와의 첫 만남.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+	int x = 2;	// x is a variable, 2 is a literal.
+}
+
---
+++ Chapter1/1_9 기본적인 서식 맞추기/1_9 기본적인 서식 맞추기.cpp
@@ -0,0 +1,20 @@
+﻿// 1_9 기본적인 서식 맞추기.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    int my_v       = 1;
+    int x          = 4;
+    int num_apples = 123;
+
+    // this is important
+    int x = 1 + 2;
+
+    // this is important, too
+    int y = 3 + 4;
+
+    return 0;
+}
+
