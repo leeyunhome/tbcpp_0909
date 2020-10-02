eb640cc31dfea2c862c06851971a12f8bf4b6f85
leeyunhome <leeyunhome@daum.net>
2020-10-02 17:42:00 +0900

    add 3.2 arithmetic operators

---
+++ Chapter3/3_2 arithmetic operators/3_2 arithmetic operators.cpp
@@ -0,0 +1,30 @@
+// 3_2 arithmetic operators.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	//int x = 1;
+	//int y = -x;
+
+	int x = 7;
+	int y = 4;
+
+	int z = x;
+
+	//cout << x / y << endl;
+	//cout << float(x) / y << endl;
+	//cout << x / float(y) << endl;
+	//cout << float(x) / float(y) << endl;
+
+	//int z = x % y;
+
+	//cout << x / y << " " << z << endl;
+
+
+	return 0;
+}
+
