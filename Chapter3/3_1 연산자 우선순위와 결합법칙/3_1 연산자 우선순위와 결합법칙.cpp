cc0b5e29f61bab74230f5ddc8e565c295468a301
leeyunhome <leeyunhome@daum.net>
2020-10-02 17:10:28 +0900

    add 3.1 연산자 우선순위와 결합법칙

---
+++ Chapter3/3_1 연산자 우선순위와 결합법칙/3_1 연산자 우선순위와 결합법칙.cpp
@@ -0,0 +1,21 @@
+﻿// 3_1 연산자 우선순위와 결합법칙.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	//int x = 4 + (2 * 3);
+
+	//int x = 3 * 4 / 2;
+	
+	int x = std::pow(2, 3);
+
+	cout << x << endl;
+
+	return 0;
+}
+
