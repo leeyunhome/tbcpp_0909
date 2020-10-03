608ae48659fcaef48f73a37ff4064cf4f1d6486b
leeyunhome <leeyunhome@daum.net>
2020-10-03 15:31:04 +0900

    add struct Person with error

---
+++ Chapter4/4_9 자료형에게 가명 붙여주기/4_9 자료형에게 가명 붙여주기.cpp
@@ -0,0 +1,20 @@
+﻿// 4_9 자료형에게 가명 붙여주기.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+#include <cstdint>
+
+int main()
+{
+	using namespace std;
+
+	//typedef vector<pair<string, int>> pairlist_t;
+	using pairlist_t = vector<pair<string, int>>;
+
+	pairlist_t pairlist1;
+	pairlist_t pairlist2;
+
+	return 0;
+}
+
