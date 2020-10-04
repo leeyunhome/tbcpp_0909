bd91e503f82802b5f1be528413c71af7ac193981
leeyunhome <leeyunhome@daum.net>
2020-10-04 10:47:09 +0900

    7.5 다양한 반환 값들

---
+++ Chapter7/7_5 다양한 반환 값들(값, 참조, 주소, 구조체, 튜플)/7_5 다양한 반환 값들(값, 참조, 주소, 구조체, 튜플).cpp
@@ -0,0 +1,20 @@
+﻿// 7_5 다양한 반환 값들(값, 참조, 주소, 구조체, 튜플).cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int* getValue(int x)
+{
+	int value = x * 2;
+	return &value;
+}
+
+int main()
+{
+	int value = *getValue(3);
+
+	return 0;
+}
+
