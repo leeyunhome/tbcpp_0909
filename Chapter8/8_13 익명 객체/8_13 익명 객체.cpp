8adbc1b773e9b295da9cebf59440d3182322dce8
leeyunhome <leeyunhome@daum.net>
2020-10-17 13:59:06 +0900

    add 8.13 익명객체

---
+++ Chapter8/8_13 익명 객체/8_13 익명 객체.cpp
@@ -0,0 +1,62 @@
+﻿// 8_13 익명 객체.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+class A
+{
+public:
+	int m_value;
+
+	A(const int& input)
+		: m_value{input}
+	{
+		cout << "Constructor" << endl;
+	}
+
+	~A()
+	{
+		cout << "Destructor" << endl;
+	}
+
+	void printDouble()
+	{
+		cout << m_value * 2 << endl;
+	}
+};
+
+class Cents
+{
+public:
+	int m_cents;
+
+	Cents(int cents)
+	{
+		m_cents = cents;
+	}
+
+	int getCents() const { return m_cents; }
+};
+
+Cents add(const Cents& c1, const Cents& c2)
+{
+	return Cents(c1.getCents() + c2.getCents());
+}
+
+int main()
+{
+	cout << add(Cents(6), Cents(8)).getCents() << endl;
+	cout << int(6) + int(8) << endl;
+	//A a(1);
+	//a.printDouble();
+	//A(1).printDouble();
+
+	//a.print();
+	
+	//A().print();
+	//A().print();
+
+	return 0;
+}
+
