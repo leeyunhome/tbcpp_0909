0462422b8cd3519fe4d883931bfb05e2f5380efe
leeyunhome <leeyunhome@daum.net>
2020-10-17 13:22:27 +0900

    add 8.12 친구함수와 클래스

---
+++ Chapter8/8_12 친구함수와 클래스 friend/8_12 친구함수와 클래스 friend.cpp
@@ -0,0 +1,40 @@
+﻿// 8_12 친구함수와 클래스 friend.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class A;	// forward declaration
+
+class B
+{
+private:
+	int m_value = 2;
+
+public:
+	void doSomething(A& a);
+};
+
+class A
+{
+private:
+	int m_value = 1;
+
+	//friend class B;
+	friend void B::doSomething(A& a);
+};
+
+void B::doSomething(A& a)
+{
+	cout << a.m_value << endl;
+}
+
+int main()
+{
+	A a;
+	B b;
+	b.doSomething(a);
+
+	return 0;
+}
