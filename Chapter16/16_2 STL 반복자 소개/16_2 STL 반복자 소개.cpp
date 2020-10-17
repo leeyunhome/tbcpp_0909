000bb7986455dd9e90460d4130afe62af2865950
leeyunhome <leeyunhome@daum.net>
2020-10-17 15:40:39 +0900

    add 16_2 STL 반복자 소개

---
+++ Chapter16/16_2 STL 반복자 소개/16_2 STL 반복자 소개.cpp
@@ -0,0 +1,29 @@
+﻿// 16_2 STL 반복자 소개.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+	map<int, char> container;
+	for (int i = 0; i < 10; ++i)
+		container.insert(make_pair(i, 65 + i));
+
+	map<int, char>::const_iterator itr;
+	itr = container.begin();
+	while (itr != container.end())
+	{
+		//cout << *itr << " ";
+		cout << itr->first << " " << itr->second << endl;
+		++itr;
+	}
+	cout << endl;
+
+	return 0;
+}
---
+++ Chapter9/9_1 산술 연산자 오버로딩 하기/9_1 산술 연산자 오버로딩 하기.cpp
@@ -0,0 +1,53 @@
+﻿// 9_1 산술 연산자 오버로딩 하기.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+class Cents
+{
+private:
+	int m_cents;
+
+public:
+	Cents(int cents = 0)
+	{
+		m_cents = cents;
+	}
+
+	int getCents() const
+	{
+		return m_cents;
+	}
+
+	int& getCents()
+	{
+		return m_cents;
+	}
+
+	// =, [], (), ->
+	Cents operator + (const Cents& c2);
+};
+
+Cents Cents::operator + (const Cents& c2)
+{
+	return Cents(this->m_cents + c2.m_cents);
+}
+//Cents operator + (const Cents& c1, const Cents& c2)
+//{
+//	return Cents(c1.m_cents + c2.m_cents);
+//}
+
+int main()
+{
+	Cents cents1(6);
+	Cents cents2(8);
+
+	cout << (cents1 + cents2 + Cents(6) + Cents(10) +
+		Cents(100)).getCents() << endl;
+
+	// ?: :: sizeof . .*
+	// ^
+
+	return 0;
+}
