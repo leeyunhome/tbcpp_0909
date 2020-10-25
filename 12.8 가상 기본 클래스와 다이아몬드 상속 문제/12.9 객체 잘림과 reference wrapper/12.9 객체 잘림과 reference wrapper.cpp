43c45172358da4cea2f137b24f0511c32379cb2a
leeyunhome <leeyunhome@daum.net>
2020-10-25 14:21:31 +0900

    add 12.9 객체 잘림과 reference wrapper

---
+++ 12.8 가상 기본 클래스와 다이아몬드 상속 문제/12.9 객체 잘림과 reference wrapper/12.9 객체 잘림과 reference wrapper.cpp
@@ -0,0 +1,59 @@
+﻿// 12.9 객체 잘림과 reference wrapper.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+#include <functional>
+
+using namespace std;
+
+class Base
+{
+public:
+	int m_i = 0;
+
+	virtual void print()
+	{
+		cout << "I'm Base" << endl;
+	}
+};
+
+class Derived : public Base
+{
+public:
+	int m_j = 1;
+
+	virtual void print() override
+	{
+		cout << "I'm derived" << endl;
+	}
+};
+
+void doSomething(Base b)
+{
+	b.print();
+}
+
+int main()
+{
+	Base b;
+	Derived d;
+
+	//std::vector<Base&> my_vec;
+	std::vector<std::reference_wrapper<Base>> my_vec;
+	my_vec.push_back(b);
+	my_vec.push_back(d);
+
+	for (auto& e : my_vec)
+		e.get().print();
+
+	//Derived d;
+	////Base b;
+	////b = d;
+
+	////b.print();
+	//doSomething(d);
+
+	return 0;
+}
+
