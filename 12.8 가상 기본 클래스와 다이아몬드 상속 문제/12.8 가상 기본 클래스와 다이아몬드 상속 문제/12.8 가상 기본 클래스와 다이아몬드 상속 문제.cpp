953dc049e3c059e973aa492aa23e8c1a6b10a242
leeyunhome <leeyunhome@daum.net>
2020-10-25 14:03:42 +0900

    add 12.8 가상 기본 클래스와 다이아몬드 상속 문제

---
+++ 12.8 가상 기본 클래스와 다이아몬드 상속 문제/12.8 가상 기본 클래스와 다이아몬드 상속 문제/12.8 가상 기본 클래스와 다이아몬드 상속 문제.cpp
@@ -0,0 +1,57 @@
+﻿// 12.8 가상 기본 클래스와 다이아몬드 상속 문제.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+class PoweredDevice
+{
+public:
+	int m_i;
+
+	PoweredDevice(int power)
+	{
+		cout << "PoweredDevice: " << power << "\n";
+	}
+};
+
+class Scanner : virtual public PoweredDevice
+{
+public:
+	Scanner(int scanner, int power)
+		: PoweredDevice(power)
+	{
+		cout << "Scanner: " << scanner << "\n";
+	}
+};
+
+class Printer : virtual public PoweredDevice
+{
+public:
+	Printer(int printer, int power)
+		: PoweredDevice(power)
+	{
+		cout << "Printer: " << printer << "\n";
+	}
+};
+
+class Copier : public Scanner, public Printer
+{
+public:
+	Copier(int scanner, int printer, int power)
+		: Scanner(scanner, power), Printer(printer, power),
+		PoweredDevice(power)
+	{
+
+	}
+};
+
+int main()
+{
+	Copier cop(1, 2, 3);
+
+	cout << &cop.Scanner::PoweredDevice::m_i << endl;
+	cout << &cop.Printer::PoweredDevice::m_i << endl;
+
+	return 0;
+}
