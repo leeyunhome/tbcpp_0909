7965f02385e3f6960d69591bc8c1fe12850f318d
leeyunhome <leeyunhome@daum.net>
2020-10-25 15:43:39 +0900

    add 12.11 유도 클래스에서 출력 연산자 사용하기

---
+++ 12.8 가상 기본 클래스와 다이아몬드 상속 문제/12.11 유도 클래스에서 출력 연산자 사용하기/12.11 유도 클래스에서 출력 연산자 사용하기.cpp
@@ -0,0 +1,47 @@
+﻿// 12.11 유도 클래스에서 출력 연산자 사용하기.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+class Base
+{
+public:
+	Base(){}
+
+	friend std::ostream& operator << (std::ostream& out, const Base& b)
+	{
+		return b.print(out);
+	}
+
+	virtual std::ostream& print(std::ostream& out) const
+	{
+		out << "Base";
+		return out;
+	}
+};
+
+class Derived : public Base
+{
+public:
+	Derived() {}
+
+	virtual std::ostream& print(std::ostream& out) const override
+	{
+		out << "Derived";
+		return out;
+	}
+};
+
+int main()
+{
+	Base b;
+	std::cout << b << '\n';
+
+	Derived d;
+	std::cout << d << '\n';	// note that this works even with no operator << that
+
+	Base& bref = d;
+	std::cout << bref << '\n';
+
+	return 0;
+}
