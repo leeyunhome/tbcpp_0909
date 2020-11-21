27d4210d4e02a2394a6378d487c83f277a51a874
leeyunhome <leeyunhome@daum.net>
2020-11-22 05:30:47 +0900

    add 18.5 정규 표현식 소개

---
+++ chapter18/18.4 흐름 상태와 입력 유효성 검증/18.4 흐름 상태와 입력 유효성 검증.cpp
@@ -0,0 +1,81 @@
+﻿// 18.4 흐름 상태와 입력 유효성 검증.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cctype>
+#include <string>
+#include <bitset>
+using namespace std;
+
+void printCharacterClassification(const int & i)
+{
+	cout << boolalpha;
+	cout << "isalnum " << bool(std::isalnum(i)) << endl;
+	cout << "isblank " << bool(std::isblank(i)) << endl;
+	cout << "isdigit " << bool(std::isdigit(i)) << endl;
+	cout << "islower " << bool(std::islower(i)) << endl;
+	cout << "isupper " << bool(std::isupper(i)) << endl;
+}
+
+void printStates(const std::ios& stream) 
+{
+	cout << boolalpha;
+	cout << "good()=" << stream.good() << endl;
+	cout << "eof()=" << stream.eof() << endl;
+	cout << "fail()=" << stream.fail() << endl;
+	cout << "bad()=" << stream.bad() << endl;
+}
+
+void classifyCharacters(const string& str)
+{
+	//bool ok_flag = true;
+
+	for (auto e : str)
+	{
+		cout << e << endl;
+		std::cout << "isdigit " << std::isdigit(e) << endl;
+		std::cout << "isblank " << std::isblank(e) << endl;
+		std::cout << "isalpha " << std::isalpha(e) << endl;
+	}
+		//if (!std::isdigit(e))
+		//{
+		//	ok_flag = false;
+		//	break;
+		//}
+
+	//return ok_flag;
+}
+
+int main()
+{
+	//while (true)
+	//{
+	//	char i;
+	//	cin >> i;
+
+	//	printStates(cin);
+
+	//	//cout << i << endl;
+	//	/*cout << boolalpha;
+	//	cout << std::bitset<8>(cin.rdstate()) << endl;
+	//	cout << std::bitset<8>(cin.istream::goodbit) << endl;
+	//	cout << std::bitset<8>(cin.istream::failbit) << endl;
+	//	cout << !bool((cin.rdstate() & std::istream::failbit) != 0) << endl;*/
+	//	//cout << (cin.rdstate() == std::istream::goodbit) << endl;
+
+	//	printCharacterClassification(i);
+
+	//	cin.clear();
+	//	cin.ignore(1024, '\n');
+	//	cout << endl;
+	//}
+
+	cout << boolalpha;
+	classifyCharacters("1234");
+	classifyCharacters("a 1234");
+
+	// regular expression
+
+	return 0;
+}
+
---
+++ chapter18/18.5 정규 표현식 소개/18.5 정규 표현식 소개.cpp
@@ -0,0 +1,43 @@
+﻿// 18.5 정규 표현식 소개.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <regex>
+
+using namespace std;
+
+int main()
+{
+	//regex re("\\d+");
+	//regex re("[ab]");
+	//regex re("[[:digit:]]{3}");
+	//regex re("[A-Z]+");
+	//regex re("[A-Z]{1,5}");
+	regex re("([0-9]{1})([@])([0-9]{1,4})");
+
+	string str;
+
+	while (true)
+	{
+		getline(cin, str);
+
+		if (std::regex_match(str, re))
+			cout << "Match" << endl;
+		else
+			cout << "No match" << endl;
+
+		// print matches
+		{
+			auto begin = std::sregex_iterator(str.begin(), str.end(), re);
+			auto end = std::sregex_iterator();
+			for (auto itr = begin; itr != end; ++itr)
+			{
+				std::smatch match = *itr;
+				cout << match.str() << " ";
+			}
+			cout << endl;
+		}
+
+		cout << endl;
+	}
+}
