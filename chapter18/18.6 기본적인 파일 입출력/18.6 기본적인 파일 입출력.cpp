4cf31737e61e927c07b2953ab7ee01a0b38fb95e
leeyunhome <leeyunhome@daum.net>
2020-11-22 06:41:23 +0900

    add 18.6 기본적인 파일 입출력

--- chapter18/18.6 기본적인 파일 입출력/18.6 기본적인 파일 입출력.cpp
+++ chapter18/18.6 기본적인 파일 입출력/18.6 기본적인 파일 입출력.cpp
@@ -12,9 +12,9 @@ using namespace std;
 int main()
 {
 	// writing
-	if (false)
+	if (true)
 	{
-		ofstream ofs("my_first_file.dat");// ios::app, ios::binary
+		ofstream ofs("my_first_file.dat", ios::app);// ios::app, ios::binary
 		// ofs.open("my_frist_file.dat");
 
 		if (!ofs)
@@ -23,8 +23,23 @@ int main()
 			exit(1);
 		}
 
-		ofs << "Line 1" << endl;
-		ofs << "Line 2" << endl;
+		//ofs << "Line 1" << endl;
+		//ofs << "Line 2" << endl;
+
+		//const unsigned num_data = 10;
+		//ofs.write((char*)&num_data, sizeof(num_data));
+
+		//for (int i = 0; i < num_data; ++i)
+		//	ofs.write((char*)&i, sizeof(i));
+		
+		stringstream ss;
+		ss << "Line 3" << endl;
+		ss << "Line 4" << endl;
+		string str = ss.str();
+
+		unsigned str_length = str.size();
+		ofs.write((char*)&str_length, sizeof(str_length));
+		ofs.write(str.c_str(), str_length);
 
 		//ofs.close(); // not necessary
 	}
@@ -40,13 +55,35 @@ int main()
 			exit(1);
 		}
 
-		while (ifs)
-		{
-			std::string str;
-			getline(ifs, str);
+		//while (ifs)
+		//{
+		//	std::string str;
+		//	getline(ifs, str);
 
-			std::cout << str << endl;
-		}
+		//	std::cout << str << endl;
+		//}
+
+		//unsigned num_data = 0;
+		//ifs.read((char*)&num_data, sizeof(num_data));
+
+		//for (unsigned i = 0; i < num_data; ++i)
+		//{
+		//	int num;
+		//	ifs.read((char*)&num, sizeof(num));
+
+		//	std::cout << num << endl;
+		//}
+
+		unsigned str_len = 0;
+		ifs.read((char*)&str_len, sizeof(str_len));
+
+		string str;
+		str.resize(str_len);
+		ifs.read(&str[0], str_len);
+
+		cout << str << endl;
+
+		//ifs.close(); // not necessary
 	}
 	return 0;
 }
