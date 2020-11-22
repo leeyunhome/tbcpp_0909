823a6a8d152dd7d98915ad46c83d2d38b2a1e935
leeyunhome <leeyunhome@daum.net>
2020-11-23 02:03:13 +0900

    19.3 std thread와 멀티쓰레딩 기초

--- Chapter19/19.3 std thread와 멀티쓰레딩 기초/19.3 std thread와 멀티쓰레딩 기초.cpp
+++ Chapter19/19.3 std thread와 멀티쓰레딩 기초/19.3 std thread와 멀티쓰레딩 기초.cpp
@@ -10,6 +10,8 @@
 
 using namespace std;
 
+mutex mtx;	// mutual exclusion
+
 int main()
 {
 	auto work_func = [](const string& name) -> void
@@ -17,13 +19,19 @@ int main()
 		for (int idx = 0; idx < 5; ++idx)
 		{
 			std::this_thread::sleep_for(std::chrono::milliseconds(100));
+			cout << 1 + 2 << endl;
 
+			mtx.lock();
 			cout << name << " " << std::this_thread::get_id() << " is working " << idx << endl;
+			mtx.unlock();
 		}
 	};
 
-	work_func("leehyerim");
-	work_func("yuri");
+	std::thread t1 = std::thread(work_func, "Jackjack");
+	std::thread t2 = std::thread(work_func, "Dash");
+
+	t1.join();
+	t2.join();
 
 	return 0;
 }
