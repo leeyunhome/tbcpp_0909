ed2f44d3c57f8fc6acb25b65a5fca302be6fa096
leeyunhome <leeyunhome@daum.net>
2020-11-22 09:30:28 +0900

    add 19.2 C++ 17 함수에서 여러 개의 리턴값 반환하기

---
+++ Chapter19/19.2 C++ 17 함수에서 여러 개의 리턴값 반환하기/19.2 C++ 17 함수에서 여러 개의 리턴값 반환하기.cpp
@@ -0,0 +1,24 @@
+﻿// 19.2 C++ 17 함수에서 여러 개의 리턴값 반환하기.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <tuple>
+
+using namespace std;
+
+auto my_func()
+{
+	return tuple(123, 456, 789, 10);
+}
+
+int main()
+{
+	auto [a, b, c, d] = my_func();
+
+	cout << a << " " << b << " " << c << " " << d << endl;
+	//auto result = my_func();
+
+	//cout << std::get<0>(result) << " " << get<1>(result) << " " << get<2>(result) << endl;
+
+	return 0;
+}
