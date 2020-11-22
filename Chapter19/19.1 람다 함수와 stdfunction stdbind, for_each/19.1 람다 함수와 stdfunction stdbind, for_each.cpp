09bc26936e0b609a7633d2227b2411a76848b937
leeyunhome <leeyunhome@daum.net>
2020-11-22 09:13:58 +0900

    add 19.1 람다 함수와 std::function std::bind, for_each
    현대적 c++에서 아주 사용량이 많아지고 있는 익명함수(무명함수)라고도 불리는 람다펑션, 그리고 추가적으로 함수포인터를 일반화시켜준  그리고 bind까지 해줄 수 있는 std::function에 대해서 설명

---
+++ Chapter19/19.1 람다 함수와 stdfunction stdbind, for_each/19.1 람다 함수와 stdfunction stdbind, for_each.cpp
@@ -0,0 +1,72 @@
+﻿// 19.1 람다 함수와 stdfunction stdbind, for_each.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+void goodbye(const string& s)
+{
+	cout << "Goodbye " << s << endl;
+}
+
+class Object
+{
+public:
+	void hello(const string& s)
+	{
+		cout << "Hello " << s << endl;
+	}
+};
+
+int main()
+{
+	// lambda-introducer
+	// lambda-parameter-declaration
+	// lambda-return-type-clause
+	// compound-statement
+	auto func = [](const int& i) -> void { cout << "Hello, World!" << " " << i << endl; };
+
+	func(123);
+
+	[](const int& i) -> void { cout << "Hello, World!" << " " << i << endl; }(7);
+	
+	{
+		string name = "JackJack";
+		[=]() {std::cout << name << endl; }();
+	}
+
+	vector<int> v;
+	v.push_back(1);
+	v.push_back(2);
+
+	auto func2 = [](int val) -> void {cout << val << endl; };
+	for_each(v.begin(), v.end(), [](int val) {cout << val << endl; });
+
+	cout << []() -> int { return 1; }() << endl;
+
+	std::function<void(int)> func3 = func2;
+	func3(123);
+
+	std::function<void(void)>func4 = std::bind(func3, 456);
+	func4();
+
+	// https://en.cppreference.com/w/cpp/utility/functional/placeholders
+	{
+		Object instance;
+		auto f = std::bind(&Object::hello, &instance, std::placeholders::_1);
+
+		f(string("World"));
+
+		auto f2 = std::bind(&goodbye, std::placeholders::_1);
+
+		f2(string("World"));
+	}
+
+	return 0;
+}
+
