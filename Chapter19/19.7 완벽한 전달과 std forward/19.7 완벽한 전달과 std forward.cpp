cada484e6c0b63b8761634fd72f882b873f48872
leeyunhome <leeyunhome@daum.net>
2020-12-02 03:54:20 +0900

    add 19.7 완벽한 전달과 std::forward

---
+++ Chapter19/19.7 완벽한 전달과 std forward/19.7 완벽한 전달과 std forward.cpp
@@ -0,0 +1,51 @@
+﻿// 19.7 완벽한 전달과 std forward.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+struct MyStruct
+{
+
+};
+
+void func(MyStruct& s)
+{
+	cout << "Pass by L-ref" << endl;
+}
+
+void func(MyStruct&& s)
+{
+	cout << "Pass by R-ref" << endl;
+}
+
+//void func(MyStruct s)
+//{
+//	cout << "Pass by Value" << endl;
+//}
+
+template<typename T>
+void func_wrapper(T&& t)
+{
+	func(std::forward<T>(t));
+}
+//template<typename T>
+//void func_wrapper(T&& t)
+//{
+//	func(std::forward<T>(t));
+//}
+
+int main()
+{
+	MyStruct s;
+
+	func_wrapper(s);
+	func_wrapper(MyStruct());
+
+	//func(s);
+	//func(MyStruct());
+}
+
